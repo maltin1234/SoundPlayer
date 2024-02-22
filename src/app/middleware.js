2e2cb7e263a114dc49b448d8c41af4da1af8e6d5
Maltin Coku (External - Experis AB) <maltin.coku@ingka.com>
2024-02-22 12:27:30 +0100

    trigger

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,9 +1,9 @@
 import { NextResponse, userAgent } from 'next/server'
 
 export const config = {
-    matcher: ['/'],
+    matcher: '/',
   }
- 
+ //trigger
 export function middleware(request) {
     return NextResponse.rewrite(new URL('/about-2', request.url))
 
